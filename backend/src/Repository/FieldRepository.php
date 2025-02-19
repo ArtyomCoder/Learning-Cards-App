74d9e5f3648555bbd46d805ea3a316424713da6a
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2025-02-19 22:07:55 +0500

    feat: создал основные репозитории

---
+++ backend/src/Repository/FieldRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Field;
+
+class FieldRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Field::class);
+    }
+
+    public function save(Field $entity): void
+    {
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+    }
+
+    public function remove(Field $entity): void
+    {
+        $this->getEntityManager()->remove($entity);
+        $this->getEntityManager()->flush();
+    }
+}
